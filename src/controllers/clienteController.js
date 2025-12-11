d02b59d8367bdb185fd0b1ba70490f3604740d2b
Math3Faria <matheusteud12@gmail.com>
2025-12-11 08:23:47 -0300

    atualizando mensagem de erro adiciona cliente

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -128,7 +128,7 @@ const clienteController = {
         } catch (error) {
             console.error(error);
             res.status(500).json({
-                message: "Teve um erro dentro do código😭, verifique se teve algum dado pessoal inserido corretamente(algum valor pode ja existir no banco de dados)."
+                message: "Teve um erro dentro do código😭, verifique se teve algum dado pessoal inserido incorretamente(algum valor pode ja existir no banco de dados)."
             });
         }
     },
